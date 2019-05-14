42353161e92c438659aadb5cda21518519e047c7
753813 <753813@123-PC03.student.Tor.Loc>
2019-05-14 16:56:06 -0400

    completed scorecalc

--- src/week02/acreCalc.java
+++ src/week02/acreCalc.java
@@ -5,6 +5,8 @@ public class acreCalc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
 	}
 
 }
--- src/week02/scoreCalc.java
+++ src/week02/scoreCalc.java
@@ -1,10 +1,23 @@
 package week02;
 
+import java.util.Scanner;
+
 public class scoreCalc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+     Scanner obj = new Scanner(System.in);
+     System.out.println("Enter mark1");
+     double m1 =obj.nextDouble();
+     System.out.println("Enter mark1");
+     double m2 =obj.nextDouble();
+     System.out.println("Enter mark1");
+     double m3 =obj.nextDouble();
+     double avg = (m1+m2+m3)/3;
+     System.out.println("Average : "+avg);
+     obj.close();
+     
+     
 	}
 
 }
